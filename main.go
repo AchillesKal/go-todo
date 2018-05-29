96ade24a473ed003cc16f9a1f8d79d265bff03b9
Achilles Kaloeridis <achilleas90gr@gmail.com>
2018-05-29 09:13:26 +0300

    adds basic / handler

--- main.go
+++ main.go
@@ -5,6 +5,11 @@ import (
 	"io/ioutil"
 )
 
-func main() {
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
 
+func main() {
+	http.HandleFunc("/", handler)
+  log.Fatal(http.ListenAndServe(":8080", nil))
 }
