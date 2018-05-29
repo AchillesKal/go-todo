f8f05f1dafabe6c65ae2b8ecb5e06086d46f0ebb
Achilles Kaloeridis <achilleas90gr@gmail.com>
2018-05-29 09:22:54 +0300

    renames handler to indexHandler

--- main.go
+++ main.go
@@ -6,13 +6,13 @@ import (
 		"html/template"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func indexHandler(w http.ResponseWriter, r *http.Request) {
 	var data = ""
 	t, _ := template.ParseFiles("templates/index.html")
 	t.Execute(w, data)
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", indexHandler)
   log.Fatal(http.ListenAndServe(":8080", nil))
 }
