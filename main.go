c3b637c4af55923cfff98c34b145ef2e5d2fa0ea
Achilles Kaloeridis <achilleas90gr@gmail.com>
2018-05-31 02:22:21 +0300

    fixes basic fetch of tasks

--- main.go
+++ main.go
@@ -20,9 +20,7 @@ func getTasks() string {
 	database, _ := sql.Open("sqlite3", "./app.db")
 	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS tasks (id INTEGER PRIMARY KEY, title TEXT, body TEXT)")
 	statement.Exec()
-	statement, _ = database.Prepare("INSERT INTO tasks (title, body) VALUES (?, ?)")
-	statement.Exec("Task1", "Lorem ipsum")
-	rows, _ := database.Query("SELECT id, title, body FROM tasks")
+	rows, _ := database.Query("SELECT * FROM tasks")
 	var id int
 	var title string
 	var body string
@@ -38,32 +36,43 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, "404 not found.", http.StatusNotFound)
 			return
 	}
+	database, _ := sql.Open("sqlite3", "./app.db")
+
+	var id int
+	var title string
+	var body string
+	var data string
+
+	rows, _ := database.Query("SELECT * FROM tasks")
+
+	for rows.Next() {
+			fmt.Println(id)
+			fmt.Println(title)
+			fmt.Println(body)
+	}
+
+	rows.Close() //good habit to close
 
 	switch r.Method {
 	case "GET":
-		var data = getTasks()
-		t, _ := template.ParseFiles("templates/index.html")
-		t.Execute(w, data)
+		getTasks()
 	case "POST":
-		var data = getTasks()
+		getTasks()
 
 		r.ParseForm()
 		x := r.Form.Get("title")
 		y := r.Form.Get("body")
-		fmt.Println(x)
-		fmt.Println(y)
-
-
-		database, _ := sql.Open("sqlite3", "./app.db")
 
 		statement, _ := database.Prepare("INSERT INTO tasks(title, body) values(?,?)")
 		statement.Exec(x, y)
 
-		t, _ := template.ParseFiles("templates/index.html")
-		t.Execute(w, data)
 	default:
 		fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
 	}
+
+	t, _ := template.ParseFiles("templates/index.html")
+	t.Execute(w, data)
+
 }
 
 func main() {
